8d4da20e3cc9ee77cd898c80e5c033f0d4232b19
Robert McCurdy <rwmccurdy@hotmail.com>
2021-04-04 12:15:15 -0700

    Facade Pattern for both Local and URL

--- src/main/java/edu/upf/taln/dri/lib/Facade.java
+++ src/main/java/edu/upf/taln/dri/lib/Facade.java
@@ -1,38 +1,106 @@
 package edu.upf.taln.dri.lib;
 
 import edu.upf.taln.dri.lib.exception.DRIexception;
+import edu.upf.taln.dri.lib.loader.JATSloader;
 import edu.upf.taln.dri.lib.loader.PDFloader;
+import edu.upf.taln.dri.lib.loader.PlainTextLoader;
+import edu.upf.taln.dri.lib.model.Document;
 import edu.upf.taln.dri.lib.util.ModuleConfig;
 import edu.upf.taln.dri.lib.util.PDFtoTextConvMethod;
+import edu.upf.taln.dri.module.importer.SourceENUM;
+
+import java.net.URL;
 
 public class Facade {
 
-	private PDFloader setUpFactory(PDFtoTextConvMethod pdfConverterType,
-								   String propertyFilePath,
-								   ModuleConfig moduleConfig) throws DRIexception {
-		// 1) Set the full path to the Dr. Inventor Framework Property files
-		Factory.setDRIPropertyFilePath(propertyFilePath);
+    private void setUpFactory(String propertyFilePath,
+                              ModuleConfig moduleConfig) throws DRIexception {
+        // 1) Set the full path to the Dr. Inventor Framework Property files
+        Factory.setDRIPropertyFilePath(propertyFilePath);
 
-		// 2) Programmatically configure the PDF processing options (http://driframework.readthedocs.io/en/latest/Initialize/)
-		Factory.setPDFtoTextConverter(pdfConverterType);
+        Factory.setModuleConfig(moduleConfig);
 
-		Factory.setModuleConfig(moduleConfig);
+        Factory.initFramework();
 
-		Factory.initFramework();
+    }
 
-		return Factory.getPDFloader();
+    private Document loadPDF(PDFtoTextConvMethod conversionType,
+                             String propertyFilePath,
+                             ModuleConfig moduleConfig,
+                             String PDFFilePath) throws DRIexception {
+        Factory.setPDFtoTextConverter(conversionType);
+        setUpFactory(propertyFilePath, moduleConfig);
+        Document document = Factory.getPDFloader()
+                .parsePDF(PDFFilePath);
+        document.preprocess();
+        return document;
+    }
 
-	}
+    private Document loadPDF(PDFtoTextConvMethod conversionType,
+                             String propertyFilePath,
+                             ModuleConfig moduleConfig,
+                             URL URLSource) throws DRIexception {
+        Factory.setPDFtoTextConverter(conversionType);
+        setUpFactory(propertyFilePath, moduleConfig);
+        Document document = Factory.getPDFloader()
+                .parsePDF(URLSource);
+        document.preprocess();
+        return document;
+    }
 
-	public PDFloader getGrobidPdfLoader(String propertyFilePath,
-										 ModuleConfig moduleConfig) throws DRIexception {
-		return setUpFactory(PDFtoTextConvMethod.GROBID, propertyFilePath, moduleConfig);
-	}
+    public Document loadLocalPDFUsingGrobid(String propertyFilePath,
+                                            ModuleConfig moduleConfig,
+                                            String PDFFilePath) throws DRIexception {
+        return loadPDF(PDFtoTextConvMethod.GROBID, propertyFilePath, moduleConfig, PDFFilePath);
+    }
 
-	public PDFloader getPdfxPdfLoader(String propertyFilePath,
-										ModuleConfig moduleConfig) throws DRIexception {
-		return setUpFactory(PDFtoTextConvMethod.PDFX, propertyFilePath, moduleConfig);
-	}
+    public Document loadLocalPDFUsingPdfx(String propertyFilePath,
+                                          ModuleConfig moduleConfig,
+                                          String PDFFilePath) throws DRIexception {
+        return loadPDF(PDFtoTextConvMethod.PDFX, propertyFilePath, moduleConfig, PDFFilePath);
+    }
 
+    public Document loadLocalXMLJats(String propertyFilePath,
+                                     ModuleConfig moduleConfig,
+                                     String JATSFilePath) throws DRIexception {
+        setUpFactory(propertyFilePath, moduleConfig);
+        Document document = Factory.getJATSloader()
+                .parseJATS(JATSFilePath);
+        document.preprocess();
+        return document;
+    }
+
+    public Document loadLocalPlainText(String textToParse,
+                                       String documentName,
+                                       String propertyFilePath,
+                                       ModuleConfig moduleConfig) throws DRIexception {
+        setUpFactory(propertyFilePath, moduleConfig);
+        Document document = Factory.getPlainTextLoader()
+                .parseString(textToParse, documentName);
+        document.preprocess();
+        return document;
+    }
+
+    public Document loadRemotePDFUsingGrobid(String propertyFilePath,
+                                             ModuleConfig moduleConfig,
+                                             URL URLSource) throws DRIexception {
+        return loadPDF(PDFtoTextConvMethod.GROBID, propertyFilePath, moduleConfig, URLSource);
+    }
+
+    public Document loadRemotePDFUsingPdfx(String propertyFilePath,
+                                           ModuleConfig moduleConfig,
+                                           URL URLSource) throws DRIexception {
+        return loadPDF(PDFtoTextConvMethod.PDFX, propertyFilePath, moduleConfig, URLSource);
+    }
+
+    public Document loadRemoteXMLJats(String propertyFilePath,
+                                      ModuleConfig moduleConfig,
+                                      URL URLSource) throws DRIexception {
+        setUpFactory(propertyFilePath, moduleConfig);
+        Document document = Factory.getJATSloader()
+                .parseJATS(URLSource);
+        document.preprocess();
+        return document;
+    }
 
 }
